4c85c857fea71c52149ff26563347c1efc18b73d
Artur Zgodziński <bivaro@gmail.com>
2025-03-22 14:41:34 +0000

    Refactor parameter handling and update document logic
    - Split the process into separate methods, each responsible for a single functionality, to improve readability and maintainability.
    - Extracted logic for updating reference parameters and arguments into dedicated methods: `UpdateRefParameter` and `UpdateRefArgument`.

--- src/Analyzers/Analyzers.CodeFixes/RefParameterAnalyzerCodeFixProvider.cs
+++ src/Analyzers/Analyzers.CodeFixes/RefParameterAnalyzerCodeFixProvider.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Immutable;
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Composition;
 using System.Linq;
 using System.Threading;
@@ -11,6 +12,7 @@ using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Editing;
 using Microsoft.CodeAnalysis.FindSymbols;
 using Microsoft.CodeAnalysis.Formatting;
+using Microsoft.CodeAnalysis.Text;
 
 
 namespace Acnutech.Analyzers
@@ -66,88 +68,112 @@ namespace Acnutech.Analyzers
             var semanticModel = await document.GetSemanticModelAsync(cancellationToken).ConfigureAwait(false);
             var methodSymbol = semanticModel.GetDeclaredSymbol(methodDeclarationSyntax, cancellationToken);
 
-            int refParameterIndex = methodDeclarationSyntax.ParameterList.Parameters.IndexOf(parameterSyntax);
-
             var methodReferences = await SymbolFinder.FindReferencesAsync(methodSymbol, document.Project.Solution, cancellationToken);
-            var methodReferencesByDocument =
+            var allReferencesByDocument =
                 methodReferences
                 .SelectMany(r => r.Locations.Select(l => (DocumentId: l.Document.Id, l.Location.SourceSpan)))
                 .Append((DocumentId: document.Id, SourceSpan: parameterSyntax.Span))
                 .GroupBy(l => l.DocumentId, l => l.SourceSpan);
 
+            var refParameterInfo = new RefParameterInfo(
+                document.Id,
+                parameterSyntax.Span.Start,
+                methodDeclarationSyntax.ParameterList.Parameters.IndexOf(parameterSyntax));
+
             var solution = document.Project.Solution;
 
-            foreach (var documentGroup in methodReferencesByDocument)
+            foreach (var documentGroup in allReferencesByDocument)
             {
-                var referenceDocument = solution.GetDocument(documentGroup.Key);
-                if (referenceDocument == null)
-                {
-                    continue;
-                }
-
-                var documentEditor = await DocumentEditor.CreateAsync(referenceDocument, cancellationToken);
-                var referenceRoot = await referenceDocument.GetSyntaxRootAsync(cancellationToken);
-
-                var spansInEditOrder = documentGroup
-                    .OrderByDescending(sp => sp.Start);
-
-                foreach (var span in spansInEditOrder)
-                {
-                    var node = referenceRoot.FindNode(span);
-
-                    if (referenceDocument.Id == document.Id && span.Start == parameterSyntax.Span.Start)
-                    {
-                        var parameterSyntax1 = (ParameterSyntax)node;
-
-                        var parameterTrivia = GetTriviaAfterNodeRemoval(refModifier, parameterSyntax1.Type.GetLeadingTrivia());
-
-                        var newParameter = parameterSyntax1.WithModifiers(SyntaxFactory.TokenList())
-                            .WithType(parameterSyntax.Type.WithLeadingTrivia(parameterTrivia))
-                            .WithAdditionalAnnotations(Formatter.Annotation);
-
-                        documentEditor.ReplaceNode(parameterSyntax, newParameter);
-                    }
-                    else
-                    {
-                        var invocationExpression = node.FirstAncestorOrSelf<InvocationExpressionSyntax>();
-
-                        var arguments = invocationExpression.ArgumentList.Arguments;
-                        if (arguments.Count <= refParameterIndex)
-                        {
-                            continue;
-                        }
-
-                        var argument = arguments[refParameterIndex];
-
-                        if (argument.RefKindKeyword.Kind() != SyntaxKind.RefKeyword
-                            || !(argument.Expression is IdentifierNameSyntax identifier))
-                        {
-                            continue;
-                        }
-
-                        var trivia = GetTriviaAfterNodeRemoval(argument.RefKindKeyword, identifier.Identifier.LeadingTrivia);
-
-                        var argumentWithoutRef =
-                            argument
-                                .WithRefKindKeyword(SyntaxFactory.Token(SyntaxKind.None))
-                                .WithLeadingTrivia(trivia)
-                                .WithAdditionalAnnotations(Formatter.Annotation);
-
-                        documentEditor.ReplaceNode(
-                            invocationExpression,
-                            invocationExpression.WithArgumentList(
-                                invocationExpression.ArgumentList.ReplaceNode(
-                                    argument,
-                                    argumentWithoutRef)));
-                    }
-
-                    var updatedDocument1 = documentEditor.GetChangedDocument();
-                    var formattedDocument = await Formatter.FormatAsync(updatedDocument1, Formatter.Annotation, cancellationToken: cancellationToken);
-                    solution = formattedDocument.Project.Solution;
-                }
+                solution = await UpdateDocument(solution, documentGroup.Key, documentGroup.Select(dg => dg), refParameterInfo, cancellationToken);
             }
 
             return solution;
+        }
+
+        private static async Task<Solution> UpdateDocument(
+            Solution solution,
+            DocumentId documentId,
+            IEnumerable<TextSpan> spans,
+            RefParameterInfo refParameterInfo,
+            CancellationToken cancellationToken)
+        {
+            var referenceDocument = solution.GetDocument(documentId);
+            if (referenceDocument == null)
+            {
+                return solution;
+            }
+
+            var documentEditor = await DocumentEditor.CreateAsync(referenceDocument, cancellationToken);
+            var documentSyntaxRoot = await referenceDocument.GetSyntaxRootAsync(cancellationToken);
+
+            var spansInEditOrder = spans
+                .OrderByDescending(sp => sp.Start);
+
+            foreach (var span in spansInEditOrder)
+            {
+                var node = documentSyntaxRoot.FindNode(span);
+
+                if (referenceDocument.Id == refParameterInfo.DocumentId && span.Start == refParameterInfo.ParameterSyntaxSpanStart)
+                {
+                    UpdateRefParameter(documentEditor, node);
+                }
+                else
+                {
+                    UpdateRefArgument(documentEditor, node, refParameterInfo.RefParameterIndex);
+                }
+            }
+
+            var updatedDocument1 = documentEditor.GetChangedDocument();
+            var formattedDocument = await Formatter.FormatAsync(updatedDocument1, Formatter.Annotation, cancellationToken: cancellationToken);
+            return formattedDocument.Project.Solution;
+        }
+
+        private static void UpdateRefParameter(DocumentEditor documentEditor, SyntaxNode node)
+        {
+            var parameterSyntax1 = (ParameterSyntax)node;
+
+            var refModifier1 = parameterSyntax1.Modifiers[0];
+            var parameterTrivia = GetTriviaAfterNodeRemoval(refModifier1, parameterSyntax1.Type.GetLeadingTrivia());
+
+            var newParameter = parameterSyntax1.WithModifiers(SyntaxFactory.TokenList())
+                .WithType(parameterSyntax1.Type.WithLeadingTrivia(parameterTrivia))
+                .WithAdditionalAnnotations(Formatter.Annotation);
+
+            documentEditor.ReplaceNode(node, newParameter);
+        }
+
+        private static void UpdateRefArgument(DocumentEditor documentEditor, SyntaxNode node, int refParameterIndex)
+        {
+            var invocationExpression = node.FirstAncestorOrSelf<InvocationExpressionSyntax>();
+
+            var arguments = invocationExpression.ArgumentList.Arguments;
+            if (arguments.Count <= refParameterIndex)
+            {
+                return;
+            }
+
+            var argument = arguments[refParameterIndex];
+
+            if (argument.RefKindKeyword.Kind() != SyntaxKind.RefKeyword
+                || !(argument.Expression is IdentifierNameSyntax identifier))
+            {
+                return;
+            }
+
+            var trivia = GetTriviaAfterNodeRemoval(argument.RefKindKeyword, identifier.Identifier.LeadingTrivia);
+
+            var argumentWithoutRef =
+                argument
+                    .WithRefKindKeyword(SyntaxFactory.Token(SyntaxKind.None))
+                    .WithLeadingTrivia(trivia)
+                    .WithAdditionalAnnotations(Formatter.Annotation);
+
+            documentEditor.ReplaceNode(
+                invocationExpression,
+                invocationExpression.WithArgumentList(
+                    invocationExpression.ArgumentList.ReplaceNode(
+                        argument,
+                        argumentWithoutRef)));
         }
 
         private static SyntaxTriviaList GetTriviaAfterNodeRemoval(SyntaxToken removedNode, SyntaxTriviaList nextLeadingTrivia)
@@ -161,5 +187,19 @@ namespace Acnutech.Analyzers
                 && trivia[0].ToString() == " "
                 ? SyntaxFactory.TriviaList()
                 : trivia;
+
+        private struct RefParameterInfo
+        {
+            public RefParameterInfo(DocumentId documentId, int parameterSyntaxSpanStart , int refParameterIndex)
+            {
+                DocumentId = documentId;
+                ParameterSyntaxSpanStart = parameterSyntaxSpanStart;
+                RefParameterIndex = refParameterIndex;
+            }
+
+            public DocumentId DocumentId { get; }
+            public int ParameterSyntaxSpanStart { get; }
+            public int RefParameterIndex { get; }
+        }
     }
 }
